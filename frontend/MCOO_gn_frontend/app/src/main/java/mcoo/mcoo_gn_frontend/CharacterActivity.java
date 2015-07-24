7d99e2cba4b3e86862b156d8b8b2c497d39014a8
Michael O Lambert <mic@la.mb>
2015-07-24 17:09:09 -0400

    Button to next page

---
+++ frontend/MCOO_gn_frontend/app/src/main/java/mcoo/mcoo_gn_frontend/CharacterActivity.java
@@ -0,0 +1,42 @@
+package mcoo.mcoo_gn_frontend;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class CharacterActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_character);
+
+        TextView textView = (TextView) findViewById(R.id.character_name);
+        textView.setText("Michaël");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_character, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
