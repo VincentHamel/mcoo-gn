f79ccce622383af0dd0ac655d60d9c98e2749511
Sebastien Pellerin <sebi.pellerin@gmail.com>
2015-07-24 19:56:22 -0400

    nodemon in details and uses of should

--- backend/test/integration/character.js
+++ backend/test/integration/character.js
@@ -6,34 +6,34 @@ if(!mongoose.connection.readyState) {
 	mongoose.connect('localhost:27017'); //Connect to our database
 }
 
-  var url = 'localhost:8080';
-  describe('Character', function() {
+var url = 'localhost:8080';
+describe('Character', function() {
     it('should return error trying to create badly form character', function(done) {
-    
-      var profile = {
-        "name" : "testCharacter",
-		"nationality" : "American",
-		"race" : "humain",
-		"profession" : "miner",
-		"class" : "warrior",
-		"belief" : "catholism",
-		"max_hp" : "12",
-		"skills" : ["hhdhd"],
-		"xp" : "1"
-      };
-	  request(url)
-      .post('/character')
-      .send(profile)
-    
-    .expect(400) //Status code
+		
+		var profile = {
+			"name" : "testCharacter",
+			"nationality" : "American",
+			"race" : "humain",
+			"profession" : "miner",
+			"class" : "warrior",
+			"belief" : "catholism",
+			"max_hp" : "12",
+			"skills" : ["hhdhd"],
+			"xp" : "1"
+		};
+		request(url)
+		.post('/character')
+		.send(profile)
+		
+		.expect(400) //Status code
 		.end(function(err, res) {
 			should.not.exist(err);
 			// this is should.js syntax, very clear
 			res.body.should.have.property('message', 'Character validation failed');
 			res.body.should.have.property('name', 'ValidationError');
 			done();
-        });
-    });
+		});
+	});
 	
 	
 	it('should return a valid character', function(done) {
@@ -50,48 +50,48 @@ if(!mongoose.connection.readyState) {
 				"max_hp" : "12",
 				"skills" : ["meditation", "hypnosis"],
 				"xp" : "1"
-			  };
+			};
 			request(url)
 			.post('/character')
 			.send(profile)
 			
 			.expect(200) //Status code
 			.end(function(err, res) {
-				  should.not.exist(err);
-				  // this is should.js syntax, very clear
-				  res.body.should.have.property('message', 'Character created!');
-				  done();	
+				should.not.exist(err);
+				// this is should.js syntax, very clear
+				res.body.should.have.property('message', 'Character created!');
+				done();	
 			});
 		});
-		  
-    });
-
+		
+	});
+	
 	it('should return error trying to create a player already exists', function(done) {
-    
-      var profile = {
-		"name" : "testChar2",
-		"nationality" : "Canadian",
-		"race" : "humain",
-		"profession" : "miner",
-		"class" : "warrior",
-		"belief" : "catholism",
-		"max_hp" : "12",
-		"skills" : ["meditation", "hypnosis"],
-		"xp" : "1"
-	  };
-	  request(url)
-      .post('/character')
-      .send(profile)
-    
-    .expect(400) //Status code
+		
+		var profile = {
+			"name" : "testChar2",
+			"nationality" : "Canadian",
+			"race" : "humain",
+			"profession" : "miner",
+			"class" : "warrior",
+			"belief" : "catholism",
+			"max_hp" : "12",
+			"skills" : ["meditation", "hypnosis"],
+			"xp" : "1"
+		};
+		request(url)
+		.post('/character')
+		.send(profile)
+		
+		.expect(400) //Status code
 		.end(function(err, res) {
 			should.not.exist(err);
 			// this is should.js syntax, very clear
 			res.body.should.have.property('message', 'E11000 duplicate key error index: test.characters.$name_1 dup key: { : \"testChar2\" }');
 			res.body.should.have.property('name', 'MongoError');
 			done();
-        });
-    });
+		});
+	});
 	
 	
 	//GET
@@ -100,26 +100,22 @@ if(!mongoose.connection.readyState) {
 		.get('/character/nonExistingCharacter')
 		.expect(404)
 		.end(function(err, res) {
-			  if (err) {
-				throw err;
-			  }
-			  // this is should.js syntax, very clear
-			  res.body.should.have.property('message', 'Character not found');
-			  done();
+			should.not.exist(err);
+			// this is should.js syntax, very clear
+			res.body.should.have.property('message', 'Character not found');
+			done();
 		});
-    });
+	});
 	
 	it('should return a valid character character', function(done) {
 		request(url)
 		.get('/character/testChar2')
 		.expect(200)
 		.end(function(err, res) {
-			  if (err) {
-				throw err;
-			  }
-			  // this is should.js syntax, very clear
-			  should.exist(res.body);
-			  done();
+			should.not.exist(err);
+			// this is should.js syntax, very clear
+			should.exist(res.body);
+			done();
 		});
-    });
+	});
 });
