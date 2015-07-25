4172e07de0de6f35d4cfceec0f8318a16e330034
DrPandemic <bipbip500@gmail.com>
2015-07-25 16:21:13 -0400

    ! FakeRepository implement CharacterRepository changes

--- frontend/MCOO_gn_frontend/app/src/main/java/mcoo/mcoo_gn_frontend/MainActivity.java
+++ frontend/MCOO_gn_frontend/app/src/main/java/mcoo/mcoo_gn_frontend/MainActivity.java
@@ -7,12 +7,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-
 public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
