2cd2181a2b0377519b5bda0623653eca03339381
Murika <erikad94@hotmail.ca>
2015-07-29 19:28:43 -0400

    did user CRUD and started tests

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ var mongoose   = require('mongoose');
 mongoose.connect('localhost:27017'); // connect to our database
 var HelloWorld = require('./app/models/hello');
 var Character = require('./app/models/character');
+var User = require('./app/models/user');
 
 //Foo bar
 var foo = require('./app/models/foo');
@@ -91,6 +92,91 @@ router.route('/character/:id')
       res.json({message: "Character not found"});
     } else
       res.json(character);
+  });
+});
+
+router.route('/user')
+// create character (accessed at POST http://localhost:8080/user)
+  .post(function(req, res) {
+  var user = new User();
+  user.name = req.body.name;
+  user.phone = req.body.phone;
+  user.hashPassword = req.body.password;
+  user.email = req.body.email;
+  user.characters = req.body.characters;
+
+  // save the character and check for errors
+  user.save(function(err) {
+
+    if (err) {
+      res.status(400);
+      res.json({ message: err.message, name: err.name })
+    }
+    else
+      res.json({ message: 'User created!' });
+  });
+});
+
+router.route('/user/:id')
+// get character (accessed at get)
+  .get(function(req, res) {
+  User.findOne({_id : req.params.id}, function(err, user) {
+    if(!user || err) {
+      res.status(404);
+      res.json({message: "User not found"});
+    } else{
+		user.hashPassword = undefined;
+		res.json(user);
+	}
+  });
+});
+
+router.route('/user')
+// update character (accessed at put http://localhost:8080/user)
+  .put(function(req, res) {
+	  
+  if(!req.body._id){
+    res.status(422);
+    res.json({message: "User missing id"});
+    return;
+  }
+	User.findOne({_id : req.body._id}, function(err, user) {
+    console.log(err);
+     if(!user || err) {
+      res.status(404);
+      res.json({message: "User not found"});
+      return;
+    } 
+    
+    if (req.body.name)user.name = req.body.name;
+    if (req.body.phone)user.phone = req.body.phone;
+    if (req.body.password)user.hashPassword = req.body.password;
+    if (req.body.email)user.email = req.body.email;
+    if (req.body.characters)user.characters = req.body.characters;
+    
+    user.save(function(err, res2) {
+      if (err) {
+        res.status(400);
+        res.json({ message: err.message, name: err.name })
+      }
+      else
+        res.json({ message: 'User updated!' , user: res2});
+    });
+  });
+});
+
+router.route('/user/:id')
+// delete character (accessed at delete)
+  .delete(function(req, res) {
+  User.findOne({_id : req.params.id}).remove(  function(err, user) {
+    if(!user || err || user.result.n <= 0) {
+      res.status(404);
+      res.json({message: "User not found"});
+    } else{
+      
+      res.json({ message: 'User deleted' });
+    }
+	
   });
 });
 
---
+++ backend/test/integration/user.js
@@ -0,0 +1,173 @@
+var should = require('should');
+var assert = require('assert');
+var request = require('supertest');
+var mongoose = require('mongoose');
+if(!mongoose.connection.readyState) {
+  mongoose.connect('localhost:27017'); //Connect to our database
+}
+
+var User = require('../../app/models/user');
+
+var url = 'localhost:8080';
+/*
+  Create : 
+    missing field
+    wrong email
+    good
+  Read :
+    wrong
+    good
+  Update :
+    wrong email
+    wrong id
+    good
+  Delete
+    good
+    wrong
+*/
+describe('User', function() {
+  describe('Create', function() {
+    beforeEach(function() {
+      User.find({name:'testUser'}).remove().exec();
+    });
+    afterEach(function() {
+      User.find({name:'testUser'}).remove().exec();
+    });
+    
+    it('should return error trying to create a user missing an attribute', function(done) {
+      var profile = {
+        "name" : "testUser",
+        "password" : "wow",
+        "email" : "legit@legit.com",
+        "characters" : []
+      };
+      request(url)
+        .post('/user')
+        .send(profile)
+        .expect(400) //Status code
+        .end(function(err, res) {
+          should.not.exist(err);
+          res.body.should.have.property('message', 'User validation failed');
+          res.body.should.have.property('name', 'ValidationError');
+          done();
+      });
+    });
+    
+    it('should return error trying to create a user with wrong email', function(done) {
+      
+      var profile = {
+        "name" : "testUser",
+        "phone" : "514 ",
+        "password" : "wow",
+        "email" : "legit@com",
+        "characters" : []
+      };
+      request(url)
+        .post('/user')
+        .send(profile)
+        .expect(400) //Status code
+        .end(function(err, res) {
+          should.not.exist(err);
+          res.body.should.have.property('message', 'User validation failed');
+          res.body.should.have.property('name', 'ValidationError');
+          done();
+      });
+    });
+    
+    it('should succeed user creation', function(done) {
+      var profile = {
+        "name" : "testUser",
+        "phone" : "514 ",
+        "password" : "wow",
+        "email" : "legit@legit.com",
+        "characters" : []
+      };
+      request(url)
+        .post('/user')
+        .send(profile)
+        .expect(200) //Status code
+        .end(function(err, res) {
+          should.not.exist(err);
+          res.body.should.have.property('message', 'User created!');
+          done();
+      });
+    });
+    
+  });
+  
+  describe('Read', function() {
+    var userId = null;
+    before(function(done) {
+      var user = new User();
+      user.name = 'testUser';
+      user.phone = 'Testland';
+      user.email = 'something@something.com';
+      user.hashPassword = 'password';
+        user.save(
+          function(err, res){
+            userId = res._id;
+            should.not.exist(err);
+            done();
+          });
+    });
+    after(function() {
+      User.find({name:'testUser'}).remove().exec();
+    });
+    
+    it('should return error trying to read wrong id', function(done) {
+      request(url)
+        .get('/user/lol')
+        .expect(404) //Status code
+        .end(function(err, res) {
+          should.not.exist(err);
+          res.body.should.have.property('message', 'User not found');
+          done();
+      });
+    });
+    
+    it('should succeed user read', function(done) {
+      request(url)
+        .get('/user/' + userId)
+        .expect(200) //Status code
+        .end(function(err, res) {
+          should.not.exist(err);
+          should.exist(res);
+          done();
+      });
+    });
+  });
+  
+  describe('Update', function() {
+    var userId = null;
+    before(function(done) {
+      var user = new User();
+      user.name = 'testUser';
+      user.phone = 'Testland';
+      user.email = 'something@something.com';
+      user.hashPassword = 'password';
+        user.save(
+          function(err, res){
+            userId = res._id;
+            should.not.exist(err);
+            done();
+          });
+    });
+    after(function() {
+      User.find({name:'testUser'}).remove().exec();
+    });
+
+    it('should be able to patch the user', function(done) {
+      console.log(userId);
+      request(url)
+        .put('/user/' + userId)
+        .send({"email" : "yolo@yolo.com"})
+        .expect(200) //Status code
+        .end(function(err, res) {
+          should.not.exist(err);
+          should.exist(res);
+          res.body.user.should.have.property('email', 'yolo@yolo.com');
+          done();
+      });
+    });
+  });
+});
